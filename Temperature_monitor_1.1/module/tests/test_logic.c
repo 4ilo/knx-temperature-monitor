330d0e2e4a35ff48a526b8132383282c4639f700
Olivier <oliviervandeneede@hotmail.com>
2020-06-14 15:06:20 +0200

    finish logic + tests

--- Temperature_monitor_1.1/module/tests/test_logic.c
+++ Temperature_monitor_1.1/module/tests/test_logic.c
@@ -1,3 +1,5 @@
+#include <string.h>
+
 #include "unity.h"
 #include "types.h"
 #include "logic.h"
@@ -7,20 +9,12 @@ output_values_t outputs;
 
 void setUp()
 {
+    memset(&inputs, 0, sizeof(input_values_t));
+    memset(&outputs, 0, sizeof(output_values_t));
+
     inputs.power_230v = 1;
     inputs.power_24v = 1;
-    inputs.lux = 0;
-    inputs.water_temp = 0;
-    inputs.outside_temp = 0;
-
-    outputs.pump_low = 0;
-    outputs.pump_high = 0;
-    outputs.tracing = 0;
-    outputs.alarm_temp_low = 0;
-    outputs.alarm_temp_high = 0;
-    outputs.alarm_230v = 0;
-    outputs.alarm_24v = 0;
-    outputs.cover_open = 0;
+    inputs.pump_active = 1;
 }
 
 void tearDown()
@@ -50,7 +44,7 @@ void test_low_outside_temp_triggers_pump(void)
     // When outside temp is again above 5c
     inputs.outside_temp = 6.0;
 
-    // Pump_low should be active
+    // Pump_low should be inactive
     ret = run_logic(&inputs, &outputs);
     TEST_ASSERT_FALSE(ret);
     TEST_ASSERT_EQUAL_INT(0, outputs.pump_low);
@@ -155,10 +149,10 @@ void test_high_outside_temp_and_lux_starts_pump(void)
     // pump low should be active
     ret = run_logic(&inputs, &outputs);
     TEST_ASSERT_FALSE(ret);
-    TEST_ASSERT_EQUAL_INT(1, outputs.pump_low);
+    TEST_ASSERT_GREATER_OR_EQUAL_INT(1, outputs.pump_low);
 
     // When outside temp is lower then 18c and light intensity is more then 1600lux
-    inputs.outside_temp = 17.5;
+    inputs.outside_temp = 17.0;
     inputs.lux = 1600;
 
     // pump low should be inactive
@@ -168,7 +162,11 @@ void test_high_outside_temp_and_lux_starts_pump(void)
 
     // When outside temp is higher then 18c and light intensity is less then 1600lux
     inputs.outside_temp = 18.5;
-    inputs.lux = 1599;
+    inputs.lux = 1601;
+    ret = run_logic(&inputs, &outputs);
+
+    inputs.outside_temp = 18.5;
+    inputs.lux = 1500;
 
     // pump low should be inactive
     ret = run_logic(&inputs, &outputs);
@@ -190,7 +188,7 @@ void test_high_water_temp_opens_cover_and_triggers_alarm(void)
     TEST_ASSERT_EQUAL_INT(1, outputs.alarm_temp_high);
 
     // When the water temp is lower then 30c
-    inputs.water_temp = 29.9;
+    inputs.water_temp = 25;
 
     // The pool cover should close
     ret = run_logic(&inputs, &outputs);
@@ -232,6 +230,168 @@ void test_timer_starts_pump_high(void)
     TEST_ASSERT_EQUAL_INT(0, outputs.pump_high);
 }
 
+void test_pump_low_has_hysteresis_for_winter(void)
+{
+    int ret = 0;
+
+    // When pump_low is active and the conditions relax just enough
+    outputs.pump_low = 1;
+    inputs.outside_temp = 5.5; // Treshold is 5.0
+
+    // Pump_low stays active
+    ret = run_logic(&inputs, &outputs);
+    TEST_ASSERT_FALSE(ret);
+    TEST_ASSERT_EQUAL_INT(1, outputs.pump_low);
+
+    // When pump_low is active and the conditions relax below the hysteresis
+    outputs.pump_low = 1;
+    inputs.outside_temp = 7; // Treshold is 5.0
+
+    // Pump_low is low
+    ret = run_logic(&inputs, &outputs);
+    TEST_ASSERT_FALSE(ret);
+    TEST_ASSERT_EQUAL_INT(0, outputs.pump_low);
+}
+
+void test_pump_low_has_hysteresis_for_summer(void)
+{
+    int ret = 0;
+
+    // When pump_low is active and the conditions relax just enough
+    outputs.pump_low = 2;
+    inputs.outside_temp = 17.5; // Treshold is 18
+    inputs.lux = 1600;
+
+    // Pump_low stays active
+    ret = run_logic(&inputs, &outputs);
+    TEST_ASSERT_FALSE(ret);
+    TEST_ASSERT_GREATER_OR_EQUAL_INT(1, outputs.pump_low);
+
+    // When pump_low is active and the conditions relax below hysteresis
+    outputs.pump_low = 2;
+    inputs.outside_temp = 16; // Treshold is 18
+    inputs.lux = 1600;
+
+    // Pump_low stays active
+    ret = run_logic(&inputs, &outputs);
+    TEST_ASSERT_FALSE(ret);
+    TEST_ASSERT_EQUAL_INT(0, outputs.pump_low);
+
+    // When pump_low is active and the conditions relax just enough
+    outputs.pump_low = 2;
+    inputs.outside_temp = 18.0;
+    inputs.lux = 1550; // Threshold is 1600
+
+    // Pump_low stays active
+    ret = run_logic(&inputs, &outputs);
+    TEST_ASSERT_FALSE(ret);
+    TEST_ASSERT_GREATER_OR_EQUAL_INT(1, outputs.pump_low);
+
+    // When pump_low is active and the conditions relax below hysteresis
+    outputs.pump_low = 2;
+    inputs.outside_temp = 18;
+    inputs.lux = 1000; // Threshold is 1600
+
+    // Pump_low stays active
+    ret = run_logic(&inputs, &outputs);
+    TEST_ASSERT_FALSE(ret);
+    TEST_ASSERT_EQUAL_INT(0, outputs.pump_low);
+}
+
+void test_pump_high_has_hysteresis(void)
+{
+    int ret = 0;
+
+    // When pump_high is active and the conditions relax just enough
+    outputs.pump_high = 1;
+    inputs.outside_temp = 3.0;
+    inputs.water_temp = 2.1; // Treshold is 2.0
+
+    // Pump_high stays active
+    ret = run_logic(&inputs, &outputs);
+    TEST_ASSERT_FALSE(ret);
+    TEST_ASSERT_EQUAL_INT(1, outputs.pump_high);
+
+    // When pump_high is active and the conditions relax below the hysteresis
+    outputs.pump_high = 1;
+    inputs.outside_temp = 3.0;
+    inputs.water_temp = 4; // Treshold is 2.0
+
+    // Pump_high is low
+    ret = run_logic(&inputs, &outputs);
+    TEST_ASSERT_FALSE(ret);
+    TEST_ASSERT_EQUAL_INT(0, outputs.pump_high);
+}
+
+void test_alarm_temp_high_has_hysteresis(void)
+{
+    int ret = 0;
+
+    // When water_temp was high and relaxes just enough
+    outputs.alarm_temp_high = 1;
+    inputs.water_temp = 29.5; // Treshold is 30
+
+    // The alarm stays active
+    ret = run_logic(&inputs, &outputs);
+    TEST_ASSERT_FALSE(ret);
+    TEST_ASSERT_EQUAL_INT(1, outputs.alarm_temp_high);
+
+    // When water_temp was high and relaxes under hysteresis
+    outputs.alarm_temp_high = 1;
+    inputs.water_temp = 25; // Treshold is 30
+
+    // The alarm is inactive
+    ret = run_logic(&inputs, &outputs);
+    TEST_ASSERT_FALSE(ret);
+    TEST_ASSERT_EQUAL_INT(0, outputs.alarm_temp_high);
+}
+
+void test_alarm_temp_low_has_hysteresis(void)
+{
+    int ret = 0;
+
+    // When water_temp was low and relaxes just enough
+    outputs.alarm_temp_low = 1;
+    inputs.water_temp = 2.5; // Treshold is 2
+
+    // The alarm stays active
+    ret = run_logic(&inputs, &outputs);
+    TEST_ASSERT_FALSE(ret);
+    TEST_ASSERT_EQUAL_INT(1, outputs.alarm_temp_low);
+
+    // When water_temp was low and relaxes under hysteresis
+    outputs.alarm_temp_high = 1;
+    inputs.water_temp = 5; // Treshold is 2
+
+    // The alarm is inactive
+    ret = run_logic(&inputs, &outputs);
+    TEST_ASSERT_FALSE(ret);
+    TEST_ASSERT_EQUAL_INT(0, outputs.alarm_temp_high);
+}
+
+void test_only_react_to_water_temp_when_pump_is_active(void)
+{
+    int ret = 0;
+
+    // When pump is inactive, and water temp is to low
+    inputs.pump_active = 0;
+    inputs.water_temp = 1;
+
+    // Don't react to water_temp
+    ret = run_logic(&inputs, &outputs);
+    TEST_ASSERT_FALSE(ret);
+    TEST_ASSERT_EQUAL_INT(0, outputs.alarm_temp_low);
+
+    // When pump is inactive, and water temp is to high
+    inputs.pump_active = 0;
+    inputs.water_temp = 50;
+
+    // Don't react to water_temp
+    ret = run_logic(&inputs, &outputs);
+    TEST_ASSERT_FALSE(ret);
+    TEST_ASSERT_EQUAL_INT(0, outputs.alarm_temp_high);
+}
+
 int main(void)
 {
     UNITY_BEGIN();
@@ -244,6 +404,12 @@ int main(void)
     RUN_TEST(test_high_outside_temp_and_lux_starts_pump);
     RUN_TEST(test_high_water_temp_opens_cover_and_triggers_alarm);
     RUN_TEST(test_timer_starts_pump_high);
+    RUN_TEST(test_pump_low_has_hysteresis_for_winter);
+    RUN_TEST(test_pump_low_has_hysteresis_for_summer);
+    RUN_TEST(test_pump_high_has_hysteresis);
+    RUN_TEST(test_alarm_temp_high_has_hysteresis);
+    RUN_TEST(test_alarm_temp_low_has_hysteresis);
+    RUN_TEST(test_only_react_to_water_temp_when_pump_is_active);
 
     return UNITY_END();
 }
